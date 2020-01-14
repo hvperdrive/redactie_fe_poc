2f7d173793a2081904d975a17bbeb4928c7d032f
Andry Charlier <andry.charlier@studiohyperdrive.be>
2020-01-14 12:23:14 +0100

    feat(external-module): add and export react component

---
+++ external-module/public/index.tsx
@@ -0,0 +1,16 @@
+import React, { FC } from 'react';
+
+const ModuleRouteComponent: FC = () => (
+	<div>
+		<h1>External Module Route</h1>
+		<p>
+			Lorem ipsum dolor, sit amet consectetur adipisicing elit. Ipsum facilis incidunt in eum
+			modi pariatur accusantium. Inventore asperiores id molestiae expedita laborum iusto
+			sequi corrupti. Beatae eius atque numquam rerum?
+		</p>
+	</div>
+);
+
+export default {
+	mainRouteComponent: ModuleRouteComponent,
+};
