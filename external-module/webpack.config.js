c671e53cf8563f8208da0304d8dabfaa7bbd2a64
De Weerdt Maarten <maarten.deweerdt@cronos.be>
2020-02-21 13:10:29 +0100

    chore: added webpack config

--- external-module/webpack.config.js
+++ external-module/webpack.config.js
@@ -1,42 +1,60 @@
 const path = require('path');
 const RedactionWebpackPlugin = require('@redactie/module-webpack-plugin');
 
-module.exports = {
-	entry: './public/index.tsx',
-	output: {
-		filename: 'external-module.umd.js',
-		path: path.resolve(__dirname, 'dist'),
-		libraryTarget: 'umd',
-	},
-	module: {
-		rules: [
-			{
-				test: /\.tsx?$/,
-				exclude: /node_modules/,
-				use: {
-					loader: "babel-loader",
-					options: {
-						presets: [
-							'@babel/preset-env',
-							'@babel/preset-react',
-							'@babel/preset-typescript',
-						],
-					},
+module.exports = env => {
+	const defaultConfig = {
+		mode: 'production',
+		devtool: 'source-map',
+		entry: './public/index.tsx',
+		performance: {
+			hints: false,
+		},
+		module: {
+			rules: [
+				{
+					test: /\.ts(x)?$/,
+					use: 'ts-loader',
+					exclude: /node_modules/,
 				},
-			},
+			],
+		},
+		resolve: {
+			extensions: ['.tsx', '.ts', '.js'],
+		},
+		plugins: [
+			// add default plugins here
 		],
-	},
-	plugins: [
-		new RedactionWebpackPlugin({
-			moduleName: 'external-demo-module'
-		})
-	],
-	resolve: {
-		extensions: [".ts", ".tsx", ".js"],
-	},
-	externals: {
-		react: 'react',
-		'@redactie/redactie-core': '@redactie/redactie-core',
-		'react-router-dom': 'react-router-dom',
-	},
+		externals: {
+			react: 'react',
+			'react-dom': 'react-dom',
+			'react-router-dom': 'react-router-dom',
+			'@redactie/redactie-core': '@redactie/redactie-core',
+		},
+		output: {
+			filename: 'external-module.umd.js',
+			path: path.resolve(__dirname, 'dist'),
+			libraryTarget: 'umd',
+		},
+	};
+
+	if (env.analyse) {
+		return {
+			...defaultConfig,
+			plugins: [...defaultConfig.plugins, new BundleAnalyzerPlugin()],
+		};
+	}
+
+	if (env.prod) {
+		return {
+			...defaultConfig,
+			plugins: [
+				...defaultConfig.plugins,
+				new RedactionWebpackPlugin({
+					moduleName: 'external-demo-module',
+				}),
+			],
+		};
+	}
+
+	return defaultConfig;
 };
